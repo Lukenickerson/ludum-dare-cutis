dc79f527024bca4c8e048383948a3032a183aba5
Luke Nickerson <luke.nickerson@gmail.com>
2014-11-23 15:49:11 -0500

    Added draft of cutis extension

---
+++ chrome-extension/cutis_extension.js
@@ -0,0 +1,8 @@
+(function(){
+	var script = document.createElement('script');
+	script.id = "cutis_script";
+	script.src = "http://lukenickerson.github.io/ludum-dare-cutis/cutis.js";
+	script.async = "false";
+	//document.write(script.outerHTML);
+	document.body.appendChild(script);
+})();
